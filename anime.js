8868295900bdbd7f63dad87c70d89510dd9c2fcb
AlessandroCampo <alessandrocampo97@gmail.com>
2024-01-22 16:42:25 +0100

    feat: static base structure in html

---
+++ anime.js
@@ -0,0 +1,26 @@
+class Anime {
+    constructor(config) {
+        this.title = title
+        this.plot = plot
+        this.source = source
+    }
+}
+
+const deathNote = new Anime({
+    title: "DEATHNOTE",
+    plot: "La storia racconta di Light, un brillante studente delle superiori che trova per caso un Death Note, un quaderno di proprietà dello shinigami (un dio della morte giapponese) Riuk, e grazie a cui Light può uccidere chiunque semplicemente annotandovi sopra il nome.",
+    source: "/img/deathnote.png"
+})
+
+const aot = new Anime({
+    title: "Attack on Titan (Shingeki no Kyojin)",
+    plot: "Attack on Titan è ambientato in un mondo dove enormi creature chiamate Titani minacciano l'esistenza umana. La storia segue Eren Yeager, Mikasa Ackerman e Armin Arlert, giovani soldati che si uniscono alla Corpo di Ricerca delle Dimensioni Umane per combattere i Titani e scoprire i segreti dietro la loro origine. Man mano che la trama si sviluppa, emergono misteri legati alle origini dei Titani, al governo e alla storia stessa del mondo.",
+    source: "/img/deathnote.png"
+})
+
+const fullMetalAlchemist = new Anime({
+    title: "Fullmetal Alchemist: Brotherhood",
+    plot: "Fullmetal Alchemist: Brotherhood segue le avventure dei fratelli Elric, Edward e Alphonse, in un mondo in cui l'alchimia è una scienza avanzata. Dopo un tragico incidente durante un rituale di alchimia per riportare in vita la madre, Edward perde un braccio e una gamba, mentre Alphonse perde l'intero corpo, il cui spirito viene legato a un'armatura. I fratelli cercano la Pietra Filosofale per ripristinare i loro corpi, ma si trovano coinvolti in una cospirazione più ampia che coinvolge il governo e la verità oscura dietro la Pietra Filosofale.",
+    source: "/img/deathnote.png"
+})
+
